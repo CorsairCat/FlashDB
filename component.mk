b83e036e08c0829e6e1b5c95ff80f94e1b544e2a
Corsaircat <1274056321@qq.com>
2023-11-27 11:51:52 -0800

    update of config files

--- component.mk
+++ component.mk
@@ -3,16 +3,15 @@
 #
 COMPONENT_ADD_INCLUDEDIRS += \
 	inc \
-	port/fal/inc
+	port/fal/inc \
+	porting_inc
 
 COMPONENT_SRCDIRS += \
 	src \
 	samples \
-	port/fal/src \
-	porting
+	port/fal/src
 
 
 # COMPONENT_PRIV_INCLUDEDIRS +=
 
 # COMPONENT_ADD_LDFLAGS += -Wl,--whole-archive -l$(COMPONENT_NAME) -Wl,--no-whole-archive
-
